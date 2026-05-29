75c804cbc70f2d5240d57af186f059993a558e3b
Ravindi Wijerathna <wathsalaravi2002@gmail.com>
2026-05-29 10:23:02 +0530

    Update gender option labels in ProjectForm

--- src/ProjectForm.jsx
+++ src/ProjectForm.jsx
@@ -912,7 +912,7 @@ export default function CombinedForm() {
             <F si="ස්ත්‍රී පුරුෂ භාවය *" ta="பாலினம் *" en="Gender *">
               <select name="gender" value={companyData.gender} onChange={handleCompanyChange} style={styles.sel}>
                 <option value="">-- තෝරන්න / தெரிவு செய்யவும் / Select --</option>
-                <option value="Male">පිරිමි / ஆண் / Male</option>
+                <option value="Male">පුරුෂ / ஆண் / Male</option>
                 <option value="Female">ස්ත්‍රී / பெண் / Female</option>
                 <option value="Any">වෙනත් / எவரும் / Any</option>
               </select>
@@ -1108,7 +1108,7 @@ export default function CombinedForm() {
             <F si="ස්ත්‍රී පුරුෂ භාවය *" ta="பாலினம் *" en="Gender *">
               <select name="gender" value={personData.gender} onChange={handlePersonChange} style={styles.sel}>
                 <option value="">-- තෝරන්න / தெரிவு செய்யவும் / Select --</option>
-                <option value="Male">පිරිමි / ஆண் / Male</option>
+                <option value="Male">පුරුෂ / ஆண் / Male</option>
                 <option value="Female">ස්ත්‍රී / பெண் / Female</option>
                 <option value="Other">වෙනත් / மற்றவை / Other</option>
               </select>
